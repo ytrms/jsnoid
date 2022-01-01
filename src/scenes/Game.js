68b32ed810edf432d90af3c2792c96d9f4d8b4e6
ytrms <lorenzo.gravina@me.com>
2022-01-01 16:27:59 +0300

    implement pausing with P

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -19,7 +19,8 @@ export default class Game extends Phaser.Scene {
 
     this.states = {
       WAITING: "waiting",
-      PLAYING: "playing"
+      PLAYING: "playing",
+      PAUSED: "paused"
     }
 
     this.gameState = this.states.WAITING
@@ -45,7 +46,6 @@ export default class Game extends Phaser.Scene {
     }
 
     this.hitBrick = function (ball, brick) {
-      console.log('brick hit!');
       brick.disableBody(true, true)
 
       if (this.bricks.countActive() === 0) {
@@ -73,30 +73,50 @@ export default class Game extends Phaser.Scene {
     this.scoreBoard = this.add.text(0, 0, `Press UP to start.`)
 
     this.cursors = this.input.keyboard.createCursorKeys()
+
+    // handle pause
+    this.input.keyboard.on('keydown-P', () => {
+      if (this.gameState !== this.states.PAUSED) {
+        this.previousGameState = this.gameState
+
+        this.previousBallVelocityX = this.ball.body.velocity.x
+        this.previousBallVelocityY = this.ball.body.velocity.y
+
+        this.ball.setVelocity(0)
+        this.pausedText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height / 2, "PAUSED", { fontSize: 28 }).setOrigin(0.5)
+        this.gameState = this.states.PAUSED
+      } else if (this.gameState === this.states.PAUSED) {
+        this.gameState = this.previousGameState
+        this.ball.setVelocity(this.previousBallVelocityX, this.previousBallVelocityY)
+        this.pausedText.destroy()
+      }
+    })
   }
 
   update() {
     // Handle input
     // move paddle
-    if (this.cursors.left.isDown) {
-      if (this.paddle.x - this.paddle.width / 2 > 0) {
-        this.paddle.x -= 5
-        this.paddle.body.updateFromGameObject()
+    if (this.gameState !== this.states.PAUSED) {
+      if (this.cursors.left.isDown) {
+        if (this.paddle.x - this.paddle.width / 2 > 0) {
+          this.paddle.x -= 5
+          this.paddle.body.updateFromGameObject()
+        }
+      } else if (this.cursors.right.isDown) {
+        if (this.paddle.x + this.paddle.width / 2 < this.cameras.main.width) {
+          this.paddle.x += 5
+          this.paddle.body.updateFromGameObject()
+        }
       }
-    } else if (this.cursors.right.isDown) {
-      if (this.paddle.x + this.paddle.width / 2 < this.cameras.main.width) {
-        this.paddle.x += 5
-        this.paddle.body.updateFromGameObject()
+      // release ball when player presses up
+      if (this.gameState == this.states.WAITING && this.cursors.up.isDown) {
+        this.gameState = this.states.PLAYING
+        this.ball.setVelocity(60, -300)
+        this.ballsUsed++
+        this.scoreBoard.setText(`Balls used: ${this.ballsUsed}`)
       }
-    }
-    // release ball when player presses up
-    if (this.gameState == this.states.WAITING && this.cursors.up.isDown) {
-      this.gameState = this.states.PLAYING
-      this.ball.setVelocity(60, -300)
-      this.ballsUsed++
-      this.scoreBoard.setText(`Balls used: ${this.ballsUsed}`)
-    }
 
+    }
     // Stick ball to paddle if game is waiting for player input
     if (this.gameState == this.states.WAITING) {
       this.ball.x = this.paddle.x
